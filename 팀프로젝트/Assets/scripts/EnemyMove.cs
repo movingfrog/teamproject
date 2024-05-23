06d0df690157ed9d6de2600640837402f144ff95
Sean <a01080080329@gmail.com>
2024-05-23 15:34:43 +0900

    EnemyMove

--- 팀프로젝트/Assets/scripts/EnemyMove.cs
+++ 팀프로젝트/Assets/scripts/EnemyMove.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    private Vector3 Movevel =  Vector3.zero;
     private CapsuleCollider2D cp;
     public float enemySpeed;
+    public int enemyHp;
     public int range;
+    public float currentSpeed;
     private int NextMove;
+    public float stopDistance;
+
+    private Transform target;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
 
     private void Awake()
     {
         cp = GetComponent<CapsuleCollider2D>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-       
+        float distanceTotarget = Vector2.Distance(transform.position, target.position);
+
+        if (distanceTotarget <= range && distanceTotarget > stopDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, enemySpeed * Time.deltaTime);
+           
+        }
+    }
+
+   private void Idle()
+    {
+
     }
 }
