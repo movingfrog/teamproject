e13eafff469d127d0e720c0e7c45fee8285ec1f0
Sean <a01080080329@gmail.com>
2024-05-26 21:54:26 +0900

    ItemPickUp

--- 팀프로젝트/Assets/scripts/ItemPickUp.cs
+++ 팀프로젝트/Assets/scripts/ItemPickUp.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class ItemPickUp : MonoBehaviour
 {
-    
+    private bool isInRange = false;
+    private float range;
+
+    private void PickUp()
+    {
+        if (range <= 1.2f)
+        {
+            isInRange = true;
+            if (Input.GetKey(KeyCode.F))
+            {
+
+            }    
+        }
+    }
 }
