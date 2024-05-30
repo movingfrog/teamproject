a499f8f3b26d8dfecd2acb7fe2fe2faf33bb1c1c
Sean <a01080080329@gmail.com>
2024-05-31 00:18:38 +0900

    EnemyAttack

--- 팀프로젝트/Assets/scripts/EnemyAttack.cs
+++ 팀프로젝트/Assets/scripts/EnemyAttack.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    PlayerMove pm;
     PlayerHealth ph;
+
     public int damege;
+
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-
+            
         }
     }
 
     private void Attack()
     {
-        ph = GetComponent<PlayerHealth>();  
         
-
     }
 }
