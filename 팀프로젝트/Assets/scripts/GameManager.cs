339f46671590b4452c124100a8ec5b108fb79b63
Sean <a01080080329@gmail.com>
2024-06-17 23:45:23 +0900

    GameManager

--- 팀프로젝트/Assets/scripts/GameManager.cs
+++ 팀프로젝트/Assets/scripts/GameManager.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager _instance;
+    public static GameManager instance { get { set(); return _instance; } }
+    public static void set()
+    {
+        GameObject go = GameObject.Find("GameManager");
+        if (go == null)
+        {
+            go = new GameObject { name = "GameManager" };
+            _instance = go.AddComponent<GameManager>();
+        }
+        return;
+    }
+
+    public bool stop = false;
 }
