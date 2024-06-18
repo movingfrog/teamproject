add07629ba944a5f87ea4bc56584c5616c6872b5
Sean <a01080080329@gmail.com>
2024-06-18 15:38:14 +0900

    EnemyHp.UI

--- 팀프로젝트/Assets/scripts/EnemyHealth.cs
+++ 팀프로젝트/Assets/scripts/EnemyHealth.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float maxHealth = 10f;
     public float currentHealth;
+    public Vector3 healthBarOffset = new Vector3(0, 1f, 0);
+    [SerializeField] private Slider EnemyHP;
 
     void Start()
     {
         currentHealth = maxHealth;
+        if (EnemyHP != null)
+        {
+            EnemyHP.maxValue = maxHealth;
+            EnemyHP.value = currentHealth;
+        }
+    }
+
+    private void Update()
+    {
+        if (EnemyHP != null)
+        {
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position + healthBarOffset);
+            EnemyHP.transform.position = screenPos;
+        }
     }
 
     public void Damage(float damage)
@@ -19,10 +36,18 @@ public class EnemyHealth : MonoBehaviour
         {
             Die();
         }
+        if (EnemyHP != null)
+        {
+                EnemyHP.value = currentHealth;
+        }
     }
 
     private void Die()
     {
         Destroy(gameObject);
+        if (EnemyHP != null)
+        {
+            Destroy(EnemyHP.gameObject);
+        }
     }
 }
